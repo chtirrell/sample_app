41cd19ce56ba0e9c9c885c4c31c4f394ddb4ec06
charlestirrell <charles@prometheusresearch.com>
2011-06-12 09:27:01 -0400

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,14 @@
 class PagesController < ApplicationController
   def home
+    @title = 'Home'
   end
 
   def content
+    @title = 'Content'
+  end
+  
+  def about
+    @title = 'About'
   end
 
 end
